4a0d90d43eb1e667f4a82e6f48ae57887ac35a65
H8346 <H8346@ELEK-431-WS-21>
2016-09-08 15:11:09 +0300

    Teht1 aloitettu

---
+++ Teht1/Tarjouslaskuri.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Tarjouslaskuri.aspx.cs" Inherits="Tarjouslaskuri" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <!-- ikkunan leveys -->
+        <p>Ikkunan leveys:
+        <asp:TextBox ID="txtIkkunaLeveys" runat="server"></asp:TextBox>
+        </p>
+
+        <!-- ikkunan korkeus -->
+        <p>Ikkunan korkeus:
+        <asp:TextBox ID="txtIkkunaKorkeus" runat="server"></asp:TextBox>
+        </p>
+
+        <!-- karmipuun leveys -->
+        <p>Karmipuun leveys:
+        <asp:TextBox ID="txtKarmiLeveys" runat="server"></asp:TextBox>
+        </p>
+
+        <p>
+        <asp:Button ID="btnLaske" runat="server" Text="Laske tarjoushinta" OnClick="btnLaske_Click" />
+        </p>
+
+        <p>Ikkunan pinta-ala:
+        <asp:Label ID="lblAla" runat="server" Text="tulos1"/>
+        </p>
+
+        <p>Karmin piiri:
+        <asp:Label ID="lblKarmi" runat="server" Text="tulos2"/>
+        </p>
+
+        <p>Tarjoushinta
+        <asp:Label ID="lblHinta" runat="server" Text="tulos3"/>
+        </p>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Teht1/Tarjouslaskuri.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Configuration; //Web.Configin lukemista varten
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Tarjouslaskuri : System.Web.UI.Page
+{
+    float ikkunaLeveys;
+    float ikkunaKorkeus;
+    float karmiLeveys;
+
+    float ala;
+    float piiri;
+    float hinta;
+
+    int kate;
+    int lasinNelioHinta;
+    int alumiiniKarminJuoksumetriHinta;
+    int tyoMenekki;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        kate = Int32.Parse(ConfigurationManager.AppSettings["kate"]);
+        lasinNelioHinta = Int32.Parse(ConfigurationManager.AppSettings["lasinNelioHinta"]);
+        alumiiniKarminJuoksumetriHinta = Int32.Parse(ConfigurationManager.AppSettings["alumiiniKarminJuoksumetriHinta"]);
+        tyoMenekki = Int32.Parse(ConfigurationManager.AppSettings["tyoMenekki"]);
+    }
+
+    protected void btnLaske_Click(object sender, EventArgs e)
+    {
+        ikkunaLeveys = float.Parse(txtIkkunaLeveys.Text);
+        ikkunaKorkeus = float.Parse(txtIkkunaKorkeus.Text);
+        karmiLeveys = float.Parse(txtKarmiLeveys.Text);
+
+        lblAla.Text = LaskeAla();
+        lblKarmi.Text = LaskePiiri();
+        lblHinta.Text = LaskeHinta();
+    }
+
+    private string LaskeAla()
+    {
+        ala = ikkunaLeveys * ikkunaKorkeus;
+        return ala.ToString();
+    }
+
+    private string LaskePiiri()
+    {
+        piiri = 2 * ikkunaLeveys + 2 * ikkunaKorkeus;
+        return piiri.ToString();
+    }
+
+    private string LaskeHinta()
+    {
+        hinta = (1 + kate) * ((ala * lasinNelioHinta) + (piiri * alumiiniKarminJuoksumetriHinta) + (tyoMenekki));
+        return hinta.ToString();
+    }
+}
