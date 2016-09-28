87ed6c990011bb1b9648a64906e3a2ccb598913b
Aleksi <Aleksi@DESKTOP-9U5VM2E>
2016-09-28 18:22:16 +0300

    Teht3 valmis

---
+++ Teht3/Kalenteri.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Kalenteri.aspx.cs" Inherits="Kalenteri" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Kalenteri</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h3>Valitse päivä:</h3>
+        <asp:Button ID="btnEdellinen" runat="server" Text="< vuosi" OnClick="btnEdellinen_Click"></asp:Button>                
+        <asp:Button ID="btnSeuraava" runat="server" Text="vuosi >" OnClick="btnSeuraava_Click"></asp:Button><br />
+        <asp:Calendar ID="kalenteri" runat="server" SelectionMode="Day" OnSelectionChanged="kalenteri_SelectionChanged"></asp:Calendar><br />
+        <asp:Label runat="server" ID="lblPvm"></asp:Label><br />
+        <asp:Label runat="server" ID="lblValittu"></asp:Label><br />
+        <asp:Label runat="server" ID="lblErotus"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Teht3/Kalenteri.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Kalenteri : System.Web.UI.Page
+{
+    DateTime date;
+    const double ApproxDaysPerMonth = 30.4375;
+    const double ApproxDaysPerYear = 365.25;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            kalenteri.VisibleDate = DateTime.Today;
+            lblPvm.Text = "Tänään on: " + kalenteri.TodaysDate.ToShortDateString();
+        }
+    }
+
+    protected void kalenteri_SelectionChanged(object sender, EventArgs e)
+    {        
+        lblValittu.Text = "Valittu: " + kalenteri.SelectedDate.ToShortDateString();
+        int days = (int)kalenteri.SelectedDate.Subtract(kalenteri.TodaysDate).TotalDays;
+        int years = (int)(days / ApproxDaysPerYear);
+        days -= (int)(years * ApproxDaysPerYear);
+        int months = (int)(days / ApproxDaysPerMonth);
+        days -= (int)(months * ApproxDaysPerMonth);
+        lblErotus.Text = "Valitun päivän ja tämän päivän erotus: <b>" + years + " v, " + months + " kk, " + days + " pv." + "</b>";
+    }
+
+    protected void btnEdellinen_Click(object sender, EventArgs e)
+    {
+        date = kalenteri.VisibleDate;
+        date = date.AddYears(-1);
+        kalenteri.VisibleDate = date;
+    }
+
+    protected void btnSeuraava_Click(object sender, EventArgs e)
+    {
+        date = kalenteri.VisibleDate;
+        date = date.AddYears(1);
+        kalenteri.VisibleDate = date;
+    }
+}
