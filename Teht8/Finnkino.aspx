08e90122d88a7a318e5dd27eebb4183e613c471b
H8346 <H8346@MEDI-426-WS-10>
2016-10-13 11:09:56 +0300

    Teht8 aloitettu

---
+++ Teht8/Finnkino.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Finnkino.aspx.cs" Inherits="Finnkino" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Finnkino</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:ListBox ID="lbTeatterit" runat="server" Height="400"></asp:ListBox>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Teht8/Finnkino.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+public partial class Finnkino : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            InitTheatres();
+        }
+    }
+
+    protected void InitTheatres()
+    {
+        XmlDataSource xds = new XmlDataSource();
+        xds.DataFile = @"http://www.finnkino.fi/xml/TheatreAreas/";
+        XmlDocument doc = new XmlDocument();
+        doc = xds.GetXmlDocument();
+        XmlNodeList nodes = doc.SelectNodes("/TheatreAreas/TheatreArea");
+        string theatreID = "";
+        string theatreName = "";
+        foreach (XmlNode node in nodes)
+        {
+            theatreID = node["ID"].InnerText;
+            theatreName = node["Name"].InnerText;
+            lbTeatterit.Items.Add(new ListItem(theatreName, theatreID));
+        }
+    }
+}
