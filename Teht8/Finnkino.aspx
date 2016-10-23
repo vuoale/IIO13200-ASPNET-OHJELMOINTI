2e63d2f0d6fe7812fce034ec8a846f05d5abf2ad
Aleksi <Aleksi@DESKTOP-9U5VM2E>
2016-10-23 21:41:06 +0300

    Teht8 valmis

--- Teht8/Finnkino.aspx
+++ Teht8/Finnkino.aspx
@@ -9,7 +9,22 @@
 <body>
     <form id="form1" runat="server">
     <div>
-    <asp:ListBox ID="lbTeatterit" runat="server" Height="400"></asp:ListBox>
+        <div style="float:left">
+            <asp:ListBox ID="lbTeatterit" runat="server" Height="400" OnSelectedIndexChanged="lbTeatterit_SelectedIndexChanged" AutoPostBack="true"></asp:ListBox>
+        </div>
+        <div style="float:left">   
+            <asp:GridView ID="gvLeffat" runat="server" AutoGenerateColumns="false">
+                <RowStyle HorizontalAlign="Center" />
+                <Columns>
+                    <asp:BoundField DataField="MovieName" HeaderText="Titteli"/>
+                    <asp:BoundField DataField="MovieStartTime" HeaderText="Alkaa" />
+                    <asp:BoundField DataField="MovieEndTime" HeaderText="Päättyy"/>
+                    <asp:BoundField DataField="MovieLength" HeaderText="Pituus (min)"/>
+                    <asp:BoundField DataField="MovieGenres" HeaderText="Genret" />
+                    <asp:ImageField DataImageUrlField="MovieImage" HeaderText="Kuva"/>
+                </Columns>
+            </asp:GridView>
+        </div>
     </div>
     </form>
 </body>
--- Teht8/Finnkino.aspx.cs
+++ Teht8/Finnkino.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -18,18 +19,33 @@ public partial class Finnkino : System.Web.UI.Page
 
     protected void InitTheatres()
     {
-        XmlDataSource xds = new XmlDataSource();
-        xds.DataFile = @"http://www.finnkino.fi/xml/TheatreAreas/";
         XmlDocument doc = new XmlDocument();
-        doc = xds.GetXmlDocument();
+        doc.Load("http://www.finnkino.fi/xml/TheatreAreas/");
         XmlNodeList nodes = doc.SelectNodes("/TheatreAreas/TheatreArea");
-        string theatreID = "";
-        string theatreName = "";
         foreach (XmlNode node in nodes)
         {
-            theatreID = node["ID"].InnerText;
-            theatreName = node["Name"].InnerText;
-            lbTeatterit.Items.Add(new ListItem(theatreName, theatreID));
+            lbTeatterit.Items.Add(new ListItem(node["Name"].InnerText, node["ID"].InnerText));
         }
+    }
+
+    protected void lbTeatterit_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        DataTable dt = new DataTable();
+        dt.Columns.Add("MovieName", typeof(string));
+        dt.Columns.Add("MovieStartTime", typeof(DateTime));
+        dt.Columns.Add("MovieEndTime", typeof(DateTime));
+        dt.Columns.Add("MovieLength", typeof(string));
+        dt.Columns.Add("MovieGenres", typeof(string));
+        dt.Columns.Add("MovieImage", typeof(string));
+        XmlDocument doc = new XmlDocument();
+        doc.Load("http://www.finnkino.fi/xml/Schedule/?area=" + lbTeatterit.SelectedValue.ToString());
+        XmlNodeList nodes = doc.SelectNodes("/Schedule/Shows/Show");
+        foreach (XmlNode node in nodes)
+        {
+            string imgUrl = node.SelectSingleNode("Images/EventSmallImagePortrait").InnerText;
+            dt.Rows.Add(node["Title"].InnerText, node["dttmShowStart"].InnerText, node["dttmShowEnd"].InnerText, node["LengthInMinutes"].InnerText, node["Genres"].InnerText, imgUrl);
+        }
+        gvLeffat.DataSource = dt;
+        gvLeffat.DataBind();
     }
 }
