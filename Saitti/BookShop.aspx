b867a541d7eb89148314b500c15de88bc8de6828
H8346 <H8346@MEDI-426-WS-10>
2016-11-03 11:07:38 +0200

    EntityFramework  demo

---
+++ Saitti/BookShop.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="BookShop.aspx.cs" Inherits="BookShop" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="body" Runat="Server">
+    <asp:Button ID="btnGetBooks" runat="server" Text="Hae kirjat" OnClick="btnGetBooks_Click" />
+    <asp:Button ID="btnGetCustomers" runat="server" Text="Hae asiakkaat" OnClick="btnGetCustomers_Click" />
+    <h1 style="w3-container">KirjakaupanX kirjat</h1>
+    <asp:GridView ID="gvBooks" runat="server" />
+    <h1 style="w3-container">KirjakaupanX asiakkaat</h1>
+    <asp:GridView ID="gvCustomers" runat="server" />
+    <asp:DropDownList ID="ddlCustomers" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddlCustomers_SelectedIndexChanged"></asp:DropDownList>
+    <h3>Asiakkaan tilaukset</h3>
+    <asp:DropDownList ID="ddlOrders" runat="server" AutoPostBack="true" />
+    <!-- ilmoitukset käyttäjälle -->
+    <h2><asp:Label ID="lblMessages" runat="server" /></h2>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="footer" Runat="Server">
+</asp:Content>
+
---
+++ Saitti/BookShop.aspx.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class BookShop : System.Web.UI.Page
+{
+    protected static BookShopEntities ctx;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            //luodaan konteksti
+            ctx = new BookShopEntities();
+            FillControls();
+        }
+    }
+    #region METHODS
+    protected void GetBooks()
+    {
+        gvBooks.DataSource = ctx.Books.ToList();
+        gvBooks.DataBind();
+    }
+    protected void GetCustomers()
+    {
+        gvCustomers.DataSource = ctx.Customers.ToList();
+        gvCustomers.DataBind();
+    }
+    protected void FillControls()
+    {
+        //ui-kontrollien alustaminen
+        var result = from c in ctx.Customers
+                     orderby c.lastname
+                     select new { c.id, c.lastname };
+        //määritellään dropdownlistille mitä se esittää
+        ddlCustomers.DataSource = result.ToList();
+        ddlCustomers.DataTextField = "lastname";
+        ddlCustomers.DataValueField = "id";
+        ddlCustomers.SelectedIndex = -1;
+        ddlCustomers.DataBind();
+        ddlCustomers.Items.Insert(0, string.Empty); //lisätään tyhjä rivi
+    }
+    #endregion
+    protected void btnGetBooks_Click(object sender, EventArgs e)
+    {
+        GetBooks();
+    }
+
+    protected void btnGetCustomers_Click(object sender, EventArgs e)
+    {
+        GetCustomers();
+    }
+
+    protected void ddlCustomers_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (ddlCustomers.SelectedIndex > 0)
+        {
+            // tyhjätään tilauksetddl
+            ddlOrders.Items.Clear();
+            int cid = -1;
+            cid = Int32.Parse(ddlCustomers.SelectedValue);
+            // haetaan valittu asiakas
+            var ret = from c in ctx.Customers
+                      where c.id == cid
+                      select c;
+            var asiakas = ret.FirstOrDefault(); // otetaan 1. entiteetti
+            if (asiakas != null)
+            {
+                lblMessages.Text = string.Format("Valitsit asiakkaan {0}", asiakas.lastname);
+                // tutkitaan onko valitulla asiakkaalla tilauksia ja jos on, näytetään
+                if (asiakas.Orders.Count > 0)
+                {
+                    lblMessages.Text += string.Format(", tilauksia {0}", asiakas.Orders.Count);
+                    // täytetään ddl tilauksilla
+                    var ret2 = (from o in asiakas.Orders
+                               select new { o.odate }).ToList();
+                    ddlOrders.DataSource = ret2;
+                    ddlOrders.DataTextField = "odate";
+                    ddlOrders.DataBind();
+                    // haetaan ja näytetään tilaukset ja tilausten tilausrivit
+                    foreach (var item in asiakas.Orders)
+                    {
+                        lblMessages.Text += string.Format("<br> tilaus: {0}", item.odate.ToShortDateString());
+                        // loopitetaan tilauksen tilausrivit
+                        foreach (var or in item.Orderitems)
+                        {
+                            lblMessages.Text += string.Format("<br>- tilattu kirja {0} ", or.Book.name);
+                        }
+                    }
+                }
+                else
+                {
+                    lblMessages.Text += ", ei ole tilauksia.";
+                    ddlOrders.DataSource = null;
+                    ddlOrders.DataBind();
+                }
+            }
+        }
+    }
+}
